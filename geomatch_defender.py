d913b8a60a00d0d69a3372d995009aafb90f15d3
Izabella de Lima Catrinck <idlc.snf23@uea.edu.br>
2024-01-20 03:18:15 -0300

    paddle triângulo criado

--- geomatch_defender.py
+++ geomatch_defender.py
@@ -1,4 +1,8 @@
 import pygame
+import sys
+from pygame.cursors import *
+from pygame.locals import Rect
+from pygame.locals import *
 
 pygame.init()
 
@@ -11,9 +15,12 @@ FPS = 60
 # creating screen
 screen = pygame.display.set_mode((S_WIDTH, S_HEIGHT))
 pygame.display.set_caption('Geomatch Defender')
+
+#classe do paddle ainda com as funções mais básicas
 class PADDLE:
     direction = None
 
+    #função de início
     def __init__(self, x, y, width, height, VELOCIDADE=10):
         self.VELOCIDADE = 10
         self.width = width
@@ -24,7 +31,6 @@ class PADDLE:
         self.x = (S_WIDTH - self.width) // 2
         self.y = S_HEIGHT - self.height - 10
         self.direction = 0
-        self.rect = Rect(self.x, self.y, self.width, self.height)
 
     def move(self):
         self.direction = 0
@@ -36,3 +42,24 @@ class PADDLE:
             self.x += self.VELOCIDADE
             self.direction = 1
         self.rect.x = self.x
+
+    #função para desenhar o paddle inicialmente como um triângulo
+    def draw(self, screen):
+        vertices = [(self.x + self.width // 2, self.y),
+                    (self.x, self.y + self.height),
+                    (self.x + self.width, self.y + self.height)]
+        color = (255, 0 ,0)
+        pygame.draw.polygon(screen, color, vertices)
+
+paddle = PADDLE(0, 0, 50, 50)
+
+#GAME LOOP
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(BG)
+    paddle.draw(screen)
+    pygame.display.flip()
